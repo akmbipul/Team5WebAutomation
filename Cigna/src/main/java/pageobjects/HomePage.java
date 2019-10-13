d8a2ee18685804cd100110b20b63c7730cd4f022
Mamun Mahmud <mamunmahmud@Mamuns-MacBook-Pro.local>
2019-10-13 09:41:46 -0400

    adding two test cases to cigna

--- Cigna/src/main/java/pageobjects/HomePage.java
+++ Cigna/src/main/java/pageobjects/HomePage.java
@@ -2,7 +2,11 @@ package pageobjects;
 
 import base.CommonAPI;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+
+import javax.swing.*;
+import java.security.PublicKey;
 
 public class HomePage extends CommonAPI {
 
@@ -17,43 +21,52 @@ public class HomePage extends CommonAPI {
     WebElement internationalHealthInsurance;
     @FindBy(css = "#\\31 564310141633 > div > div.col-12.col-lg-3 > h2")
     WebElement importantBenifitUpdates;
-    @FindBy(css="#\\31 503828750839 > div.global-header > div > nav.navbar.global-nav.universal-navigation.navbar-expand-lg.navbar-white.bg-white.pt-lg-0 > div > ul:nth-child(2) > li:nth-child(1) > a")
+    @FindBy(xpath="//*[@id=\"includes-content\"]/div[1]/nav[1]/div/ul/li[1]/a")
     WebElement cignaInternational;
-    @FindBy(css="#\\31 503828750839 > div.global-header > div > nav.navbar.global-nav.universal-navigation.navbar-expand-lg.navbar-white.bg-white.pt-lg-0 > div > ul:nth-child(2) > li:nth-child(2) > a")
-    WebElement aboutCigna;
     @FindBy(css="#\\31 564310141631 > div > div > div:nth-child(2) > div > div.teaser-content.col-12.col-md-7.col-lg-12.align-self-lg-center > header > a")
     WebElement healthierKidsforOurFuture;
+    @FindBy(xpath = "//a[text()='About Us']")
+    WebElement aboutUs;
+    @FindBy(css = ".container>ul>li:nth-child(4)>a")
+     WebElement investorRelations;
+    @FindBy(xpath = "//a[text()='Cigna Overview']")
+    WebElement cignaOverview;
+
     public void clickOnIndividulasAndFamilies() {
         clickOnElement("#includes-content > div.d-none.d-lg-block > nav.navbar.global-nav.navbar-expand-lg.site-navigation-level-1 > div > ul > li:nth-child(4) > afir");
     }
-
-    public void employerBrokerLoginTest()
-    {
-        this.employerBrokerLogin.click();
-    }
-
+    public void employerBrokerLoginTest() { this.employerBrokerLogin.click(); }
     public void internationalHealthInsuranceTest(){
         internationalHealthInsurance.click();
     }
-
     public void otherSupplimentalInsuranceTest() {
         this.otherSupplimentalInsurance.click();
     }
-
     public void importantBenifitUpdatesTest() {
         this.importantBenifitUpdates.click();
     }
-
-    public void setCignaInternational(){
-        cignaInternational.click();
-    }
-    public void setAboutCigna(){
-        aboutCigna.click();
-    }
+    public void setCignaInternational(){ cignaInternational.click(); }
+    public void getAboutUsCigna(){ aboutUs.click(); }
     public void setDentalInsurance(){
         this.dentalInsurancePlan.click();
     }
     public void setHealthierKidsforOurFuture(){
         this.healthierKidsforOurFuture.click();
     }
+    public void mouseHoverOnInvestorRelations(){
+        getAboutUsCigna();
+        WebElement element=investorRelations;
+        Actions actions=new Actions(driver);
+        actions.moveToElement(element).perform();
+    }
+    public void clickOnCignaOverview(){
+        getAboutUsCigna();
+        mouseHoverOnInvestorRelations();
+        cignaOverview.click();
+
+
+    }
+
+
+
     }
--- Cigna/src/test/java/home/TestHome.java
+++ Cigna/src/test/java/home/TestHome.java
@@ -4,58 +4,51 @@ import base.CommonAPI;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pageobjects.CompanyFrofilePage;
 import pageobjects.HomePage;
 
     public class TestHome extends CommonAPI {
     HomePage homePage;
-  //  CompanyFrofilePage companyFrofilePage;
+    CompanyFrofilePage companyFrofilePage;
 
     @BeforeMethod
     public void initElements() {
         homePage = PageFactory.initElements(driver, HomePage.class);
+        companyFrofilePage=PageFactory.initElements(driver, CompanyFrofilePage.class);
     }
-   // @Test
+
+        //@Test
     public void testOfEmployerBrokerLoginButton() {
         homePage.employerBrokerLoginTest();
     }
-    @Test
+    // @Test
     public void test3() {
         homePage.internationalHealthInsuranceTest();
     }
-    @Test
-    public void test4() {
-        homePage.importantBenifitUpdatesTest();
-    }
-   // @Test
-    public void individualFamilyTest(){
-        homePage.clickOnIndividulasAndFamilies();
-    }
-    @Test
-    public void supplimentalTest(){
-
-        homePage.otherSupplimentalInsuranceTest();
-    }
-   // @Test
-    public void cignaInternationalTest(){
-        homePage.setCignaInternational();
-    }
     //@Test
-    public void aboutCignaTest(){
-        homePage.setAboutCigna();
-    }
-    @Test
+    public void test4() { homePage.importantBenifitUpdatesTest(); }
+    // @Test
+    public void supplimentalTest(){ homePage.otherSupplimentalInsuranceTest(); }
+    // @Test
     public void dentalInsuranceTest(){
         homePage.setDentalInsurance();
     }
-    @Test
+    // @Test
     public void healthierKidsForFutureTest() {
         homePage.setHealthierKidsforOurFuture();
     }
-
     //@Test
-        public void milestonesTest(){
-        //companyFrofilePage.setMilestones();
-    }
+    public void testCignaInternational() {homePage.setCignaInternational();}
+    //@Test
+    public void testAboutUsCigna(){homePage.getAboutUsCigna();}
+     @Test
+     public void testMouseHoverOnInvestorRelations(){
+        homePage.mouseHoverOnInvestorRelations();
+      }
 
+      //@Test
+        public void testClickOnCignaOverview(){
+        homePage.clickOnCignaOverview();
+      }
 
     }
